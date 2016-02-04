1c08550d9aed1eae3aabbecb393b1b6b46198dad
Alston Lin <alstonlin812@gmail.com>
2016-02-04 18:53:10 -0500

    Removed the old, now unused hardcoded templates

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -11,75 +11,7 @@ class Problem < ActiveRecord::Base
 	validates :number, presence: true
 	validates :creator, presence: true
 	validates :source, presence:
-	#--------------------------TEMPLATES------------------------------------------
-	# TODO: Have course creator make them?
-	PYTHON_PRINT_TEMPLATE = ['import sys',
-  'import platform',
-  'from subprocess import Popen, PIPE, STDOUT',
-  'failedCount = 0',
-	'def getFileName():',
-	'  assert len(sys.argv) == 2, "You must specify exactly one filepath of the executable to test"',
-  '  if platform.system() == "Windows":',
-  '    return sys.argv[1]',
-  '  else:',
-  '    return "./" + sys.argv[1]',
-	'',
-	'def getOutput(filename, inp):',
-  '	program = Popen([filename], stdout=PIPE, stdin=PIPE, stderr=STDOUT)',
-  '	for line in inp:',
-  '		program.stdin.write(line)',
-  '	return program.communicate()[0].split("\n")',
-	'',
-	'def printFail(inp, output, actual):',
-  '  print "----------FAILED----------"',
-	'  print "Input\n", inp',
-	'  print "Expected\n", output',
-	'  print "Actual\n", actual',
-	'  global failedCount',
-	'  failedCount += 1',
-	'',
-	'def testCase(filename, inp, out):',
-	'  actual = getOutput(filename, inp)',
-	'  output = out.split("\n")',
-  '  inp = inp.split("\n")',
-	'  if len(actual) == len(output):',
-	'    for i in range(len(actual)):',
-	'      if actual[i] != output[i]:',
-  '        printFail(inp, output, actual)',
-  '        return',
-	'  else:',
-	'    printFail(inp, output, actual)',
-	'',
-	'filename = getFileName()',
-	'{{each}}',
-	'testCase(filename, "{{input}}", "{{output}}")',
-	'{{end}}',
-	'if failedCount == 0:',
-  '	print "Congratulations! All the test cases has passed"',
-	'else:',
-  '	print "------------------------------"',
-  '	print "Failed ", failedCount, " tests. :("'].join("\n") + "\n"
-
-	CPP_FUNCTION_TEMPLATE = ['#include <iostream>',
-		'#include <string>',
-		'using namespace std;',
-		'int FUNCTION_HERE();',
-		'int main(){',
-		'	string inputs[{{count}}];',
-		'	{{each}}',
-		'	inputs[{{index}}] = "{{input}}";',
-		'	outputs[{{index}}] = "{{output}}";',
-		'	assert(INSERT_ASSERT_HERE);',
-		'	{{end}}',
-		'	cout << "Passed All Test Cases"',
-	'}'].join("\n") + "\n"
-
-  INSTRUCTIONS_TEMPLATE_PYTHON = ['1. If you have not already, install Python',
-    '2. Copy and Paste the code below into a new file, muriqui.py',
-    '3. Put muriqui.py in the same directory as your compiled program',
-    '4. Open the terminal and go to the directory of muriqui.py',
-    '5. Run the test by entering the following command into the terminal: python muriqui.py COMPILED_FILE_NAME'].join("\n") + "\n"
-
+	# Constant
 	MODES = {"Python" => "text/x-python", "C++" => "text/x-c++src"}
 	#------------------INSTANCE METHODS-------------------------------------------
 	def initialize(attributes={}, options={})
@@ -128,7 +60,7 @@ class Problem < ActiveRecord::Base
 			end
 		end
 		generated_source += after
-		# Subs /r/n with //n for the code
+
 		return generated_source
 	end
 
